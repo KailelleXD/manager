0e97c3a23ea0c056bdb9cce8572c0288867d89eb
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-03-17 15:53:21 -0700

    navigation between screens complete

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
 
 const RouterComponent = () => {
     return (
@@ -12,14 +14,20 @@ const RouterComponent = () => {
                         key="login" 
                         component={LoginForm} 
                         title="Please Login" 
-                        initial
                     />
                 </Scene>
-                <Scene key="main">
+                <Scene key="main" initial>
                     <Scene
+                        rightTitle="Add"
+                        onRight={() => { Actions.employeeCreate() }}
                         key="employeeList"
                         component={EmployeeList}
                         title="Employee List"
+                    />
+                    <Scene
+                        key="employeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"
                     />
                 </Scene>
             </Scene>
